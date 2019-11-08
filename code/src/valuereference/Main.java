df1a6a791b1ce278f7123f3f1a6f3a0b62d3f2a8
icodeu <icodeyou@sina.com>
2019-11-08 20:37:07 +0800

    add valuereference

---
+++ code/src/valuereference/Main.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2019 Baidu, Inc. All Rights Reserved.
+ */
+package valuereference;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // 基本类型
+        int intValue = 18;
+        changeInt(intValue);
+        System.out.println(intValue);
+
+        // 包装类型
+        Integer integerValue = 18;
+        changeInt(integerValue);
+        System.out.println(integerValue);
+
+        // String - 不可变类
+        String strValue = "CSDN";
+        changeString(strValue);
+        System.out.println(strValue);
+
+        // 自定义类
+        Person person = new Person();
+        person.setName("CSDN");
+        person.setAge(18);
+        changePerson(person);
+        System.out.println(person);
+    }
+
+    private static void changeInt(int value) {
+        value = 20;
+    }
+
+    private static void changeInteger(Integer value) {
+        value = 20;
+    }
+
+    private static void changeString(String value) {
+        value = "CSDN-changed";
+    }
+
+    private static void changePerson(Person person) {
+        person.setName("CSDN-changed");
+        person.setAge(20);
+    }
+
+}
---
+++ code/src/valuereference/Person.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2019 Baidu, Inc. All Rights Reserved.
+ */
+package valuereference;
+
+public class Person {
+
+    private String name;
+
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
